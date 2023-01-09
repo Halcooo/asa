87136e4062db9afdd7bc8d6cb6d7bad18234663e
Halid Lihovac <halid.lihovac@gmail.com>
2023-01-09 14:30:12 +0100

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,26 +1,20 @@
 <template>
   <AboutVue />
   <AboutNext />
-  <!-- <AboutChoose /> -->
   <AboutVideo />
    <AboutNums /> 
-  <!-- <AboutReserv /> -->
 </template>
 <script>
 import AboutVue from "@/components/AboutView/About.vue";
 import AboutNext from "@/components/AboutView/AboutNext.vue";
-import AboutReserv from "@/components/AboutView/AboutReserv.vue";
 import AboutNums from "@/components/AboutView/AboutNums.vue";
 import AboutMap from "@/components/AboutView/AboutMap.vue";
-import AboutChoose from "@/components/AboutView/AboutChoose.vue";
 import AboutVideo from "@/components/AboutView/AboutVideo.vue";
 export default {
   components: {
     AboutVue,
     AboutNext,
-    AboutChoose,
     AboutVideo,
-    AboutReserv,
     AboutNums,
     AboutMap,
   },
