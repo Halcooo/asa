1122fe812f2a4c9c06a2c743f6186011facfce22
Halid Lihovac <halid.lihovac@gmail.com>
2023-01-10 15:52:14 +0100

    update

--- src/i18n.js
+++ src/i18n.js
@@ -24,7 +24,7 @@ function loadLocaleMessages() {
 }
 
 export default createI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || "en",
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en",
+  locale: process.env.VUE_APP_I18N_LOCALE || "bs",
+  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "bs",
   messages: loadLocaleMessages(),
 });
