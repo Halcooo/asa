200dc45fa2713acfc7625cd98c67c12be354ff1a
Halid Lihovac <halid.lihovac@gmail.com>
2023-01-17 11:48:30 +0100

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,14 +2,16 @@
   <AboutVue />
   <AboutNext />
   <AboutVideo />
-   <AboutNums /> 
+  <AboutNums />
 </template>
 <script>
+
 import AboutVue from "@/components/AboutView/About.vue";
 import AboutNext from "@/components/AboutView/AboutNext.vue";
 import AboutNums from "@/components/AboutView/AboutNums.vue";
 import AboutMap from "@/components/AboutView/AboutMap.vue";
 import AboutVideo from "@/components/AboutView/AboutVideo.vue";
+
 export default {
   components: {
     AboutVue,
